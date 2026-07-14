9d99e6034e93a20e8935572f73ac7e504ae35175
MattRamCoder03 <matzram03@gmail.com>
2026-07-14 19:52:16 -0400

    wire/buses

--- CPU.sv
+++ CPU.sv
@@ -4,5 +4,8 @@ module CPU(input logic clk, input logic rst);
   reg[5:0] PC, AR;
   reg[7:0] DR, AC;
   reg[7:0] memory [0:3];
+
+  wire [7:0] shared_bus, MEM_bus, DR_bus;
+  wire[5:0] PC_bus,
   
 endmodule
