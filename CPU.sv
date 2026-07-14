a6d58acc07f71794ef79f4d5f0a8a093946f552b
MattRamCoder03 <matzram03@gmail.com>
2026-07-14 19:42:06 -0400

    registers made, signal names changed

--- CPU.sv
+++ CPU.sv
@@ -2,6 +2,7 @@ module CPU(input logic clk, input logic rst);
 
   reg[1:0] IR;
   reg[5:0] PC, AR;
-  reg[7:0] DR;
+  reg[7:0] DR, AC;
+  reg[3:0] memory [7:0];
   
 endmodule
--- CTRL_UNIT.sv
+++ CTRL_UNIT.sv
@@ -1,2 +1,2 @@
-module CTRL_UNIT(input logic [1:0] opcode, output logic ctrl_signals[11:0]);
+module CTRL_UNIT(input logic [1:0] opcode, output logic ctrl_sig[11:0]);
 endmodule
