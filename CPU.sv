6a4fcc8f75aab5c47a9e8f79704550b78604d3cf
MattRamCoder03 <matzram03@gmail.com>
2026-07-15 18:04:47 -0400

    Modified CPU code for icarus verilog compliant SV

--- CPU.sv
+++ CPU.sv
@@ -1,12 +1,32 @@
+module top_module ();
+	reg clk=0;
+	always #5 clk = ~clk;  // Create clock with period=10
+	initial `probe_start;   // Start the timing diagram
+
+	`probe(clk);        // Probe signal "clk"
+
+	// A testbench
+	reg start=1;
+    reg rst =1;
+	initial begin
+        #12 rst<=0;
+		$display ("Hello world! The current time is (%0d ps)", $time);
+		#50 $finish;            // Quit the simulation
+	end
+    
+    CPU test1(.clk(clk), .start(start), .rst(rst));
+
+endmodule
+
 module CPU(input logic clk, input logic start, input logic rst);
 
   reg[1:0] IR;
   reg[5:0] PC, AR;
   reg[7:0] DR, AC, MEM;
 
-  wire[1:0] next_IR;
-  wire[5:0] next_PC, next_AR;
-  wire[7:0] next_DR, next_AC;
+  logic[1:0] next_IR;
+  logic[5:0] next_PC, next_AR;
+  logic[7:0] next_DR, next_AC;
 
 reg [3:0] state, next_state;
 parameter IDLE=4'd0, FETCH1=4'd1, FETCH2=4'd2, FETCH3=4'd3, ADD1=4'd4, ADD2=4'd5;
@@ -17,7 +37,12 @@ parameter IDLE=4'd0, FETCH1=4'd1, FETCH2=4'd2, FETCH3=4'd3, ADD1=4'd4, ADD2=4'd5
   //always block for sequential and reset
 
 //reset logic
-  always_ff(posedge clk) begin 
+    
+  initial begin
+    MEM = 8'd2;
+end
+    
+  always@(posedge clk) begin 
   if (rst) begin 
         state<=IDLE;
       end 
@@ -27,7 +52,7 @@ parameter IDLE=4'd0, FETCH1=4'd1, FETCH2=4'd2, FETCH3=4'd3, ADD1=4'd4, ADD2=4'd5
     end 
 
 //combinational logic for next state
-always_comb begin 
+always@(*) begin 
   case(state) 
     IDLE: begin 
     if (start) begin 
@@ -62,61 +87,61 @@ always_comb begin
 end 
 
 //combinational logic for next outputs 
-always_comb begin 
+always@(*) begin 
   case(state) 
     IDLE: begin 
       next_AR = PC;
-      next_DR = 8'd0;
-      next_PC = 6'd0;
-      next_IR = 2'd0;
-      next_AC = 8'd0;
+      next_DR = DR;
+      next_PC = PC;
+      next_IR = IR;
+      next_AC = AC;
     end 
     FETCH1: begin 
-      next_AR = 6'd0;
+      next_AR = AR;
       next_DR = MEM;
       next_PC = PC+1;
-      next_IR = 2'd0;
-      next_AC = 8'd0;
+      next_IR = IR;
+      next_AC = AC;
     end 
     FETCH2: begin
-      next_AR = DR[5:0];
-      next_DR = 8'd0;
-      next_PC = 6'd0;
+      next_AR = {DR[5:0]};
+      next_DR = DR;
+      next_PC = PC;
       next_IR = DR[7:6];
-      next_AC = 8'd0;
+      next_AC = AC;
     end 
     FETCH3: begin       
-      next_AR = 6'd0;
+      next_AR = AR;
       next_DR = MEM;
-      next_PC = 6'd0;
-      next_IR = 2'd0;
-      next_AC = 8'd0;
+      next_PC = PC;
+      next_IR = IR;
+      next_AC = AC;
     end 
     ADD1: begin       
-      next_AR = 6'd0;
-      next_DR = 8'd0;
-      next_PC = 6'd0;
-      next_IR = 2'd0;
+      next_AR = AR;
+      next_DR = DR;
+      next_PC = PC;
+      next_IR = IR;
       next_AC = AC+DR;
     end 
     ADD2: begin 
-      next_AR = 6'd0;
-      next_DR = 8'd0;
-      next_PC = 6'd0;
-      next_IR = 2'd0;
-      next_AC = 8'd0;
+      next_AR = AR;
+      next_DR = DR;
+      next_PC = PC;
+      next_IR = IR;
+      next_AC = AC;
     end 
   endcase
 end 
 
 //sequential logic for output registering
-always_ff(posedge clk) begin 
+always@(posedge clk) begin 
   if (rst) begin 
         AR<=6'd0;
         DR<=8'd0;
         PC<=6'd0;
         IR<=2'd0;
-        AC<=8'd0
+        AC<=8'd0;
   end 
   else begin
     AR<=next_AR;
@@ -126,5 +151,14 @@ always_ff(posedge clk) begin
     AC<=next_AC;
   end 
 end
-  
+    `probe(AC);
+    `probe(PC);
+    `probe(DR);
+    `probe(IR);
+    `probe(AR);
+    `probe(MEM);
+    `probe(state);
+    `probe(next_state);
 endmodule
+
+
