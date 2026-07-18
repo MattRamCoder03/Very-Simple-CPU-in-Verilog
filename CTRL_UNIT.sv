4672cdc304415fba5c1f124bfabe418141b03bf2
MattRamCoder03 <matzram03@gmail.com>
2026-07-18 19:29:59 -0400

    Update CTRL_UNIT.sv

--- CTRL_UNIT.sv
+++ CTRL_UNIT.sv
@@ -1,15 +1,21 @@
 module CTRL_UNIT(input logic [3:0] current_state,  
 input logic [1:0] opcode, 
 output logic[1:0] next_IR, 
-output_logic[5:0] next_PC, next_AR,
+output logic[5:0] next_PC, next_AR,
 output logic[7:0] next_DR, next_AC);
 
 logic arload, pcload, pcinc, drload, acload, acinc, irload, alusel, membus, pcbus, drbus, read;
 logic ctrl_sig[11:0] ={arload,pcload,pcinc,drload,acload,acinc,irload,alusel,membus,pcbus,drbus,read};
+    
 //assign each signal to a bit in the wire 
     
 //state machine for always
 //combinational logic for next outputs (control unit)
+//txtbook control signals are asserted the state BEFORE the textbook presentations, this is so that the proper values can be latched
+//and registered cleanly on the positive edge of the next clock cycle
+//i.e. if the textbook says fetch1 asserts a specific signal, it will actually be asserted on the idle state (before fetch1) 
+//the register transfer associated with fetch 1 then cleanly occurs on the positive edge of the clock for fetch 1
+    
 always@(*) begin 
       next_AR = AR;
       next_DR = DR;
@@ -18,11 +24,14 @@ always@(*) begin
       next_AC = AC;
   case(current_state) 
     IDLE: begin 
-      next_AR = PC;
+        pcbus=1;
+        arload=1;
     end 
     FETCH1: begin 
-      next_DR = MEM;
-      next_PC = PC+1;
+        pcinc=1;
+        membus=1;
+        drload=1;
+        read=1;
     end 
     FETCH2: begin
       next_AR = {DR[5:0]};
